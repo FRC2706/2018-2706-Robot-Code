565376c77dd14f3ff8749c1adb9789b574a46a24
Kevin Lam <kevlam@gmail.com>
2018-02-03 16:07:01 -0500

    Fixes for Ryan

--- src/org/usfirst/frc/team2706/robot/commands/autonomous/experimential/curvedrive/CubicEquation.java
+++ src/org/usfirst/frc/team2706/robot/commands/autonomous/experimential/curvedrive/CubicEquation.java
@@ -2,8 +2,14 @@ package org.usfirst.frc.team2706.robot.commands.autonomous.experimential.curvedr
 
 import java.text.DecimalFormat;
 
+/**
+ * Stores and allows you to print the equation if neccesary, more useful for the console version but
+ * still valuable
+ */
 public class CubicEquation {
-    public double a,b,c,d;
+
+    // The four coefficients that need to be saved
+    public double a, b, c, d;
 
     public CubicEquation(double a, double b, double c, double d) {
         this.a = a;
@@ -11,8 +17,13 @@ public class CubicEquation {
         this.c = c;
         this.d = d;
     }
+
+    /**
+     * Prints the equation so it can be copied into desmos easily.
+     */
     public String toString() {
-        DecimalFormat df = new DecimalFormat("############.#########################################");  
+        DecimalFormat df =
+                        new DecimalFormat("############.#########################################");
         String aa = df.format(a);
         String bb = df.format(b);
         return "x = " + aa + "y^3 + " + bb + "y^2";
