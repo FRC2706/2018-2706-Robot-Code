211daea62978bad11679f9f57dddcc251d032534
eandr127 <eandr127@outlook.com>
2018-02-17 13:45:33 -0500

    Use PID to follow camera

--- src/org/usfirst/frc/team2706/robot/vision/FollowCamera.java
+++ src/org/usfirst/frc/team2706/robot/vision/FollowCamera.java
@@ -3,8 +3,13 @@ package org.usfirst.frc.team2706.robot.vision;
 import org.usfirst.frc.team2706.robot.Robot;
 
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Drives the robot in a straight line towards the target found by the camera. Used for lining up
@@ -12,6 +17,9 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class FollowCamera extends Command {
 
+    private final PIDController rotatePID;
+    private final double P = 1, I = 0, D = 0;
+    
     /**
      * Drive at a specific speed based on camera
      * 
@@ -21,19 +29,60 @@ public class FollowCamera extends Command {
      */
     public FollowCamera() {
         requires(Robot.driveTrain);
-
+        
+        rotatePID = new PIDController(P, I, D, new CameraPID(), Robot.driveTrain.getDrivePIDOutput(false, false, true));
+        
+        SmartDashboard.putNumber("P", SmartDashboard.getNumber("P", P));
+        SmartDashboard.putNumber("I", SmartDashboard.getNumber("I", I));
+        SmartDashboard.putNumber("D", SmartDashboard.getNumber("D", D));
     }
 
+    private class CameraPID implements PIDSource {
+
+        private PIDSourceType pidSource = PIDSourceType.kDisplacement;
+        
+        private final NetworkTableEntry ctrX;
+        
+        {
+            NetworkTable table = NetworkTableInstance.getDefault().getTable("vision");
+            ctrX = table.getEntry("ctrX");
+        }
+        
+        @Override
+        public void setPIDSourceType(PIDSourceType pidSource) {
+            this.pidSource = pidSource;
+            
+        }
+
+        @Override
+        public PIDSourceType getPIDSourceType() {
+            return pidSource;
+        }
+
+        @Override
+        public double pidGet() {
+            return ctrX.getDouble(0);
+        }
+        
+    }
+    
     // Called just before this Command runs the first time
     protected void initialize() {
+        rotatePID.setP(SmartDashboard.getNumber("P", P));
+        rotatePID.setI(SmartDashboard.getNumber("I", I));
+        rotatePID.setD(SmartDashboard.getNumber("D", D));
+        
+        rotatePID.setOutputRange(-0.3, 0.3);
+        rotatePID.setSetpoint(0);
+        rotatePID.enable();
     }
    
     protected void execute() {
-        NetworkTable table = NetworkTableInstance.getDefault().getTable("vision");
-        double input = table.getEntry("ctrX").getDouble(0);
-        double rotateVal = (input < 0 ? -1 : 1) * Math.sqrt(Math.abs(input));
-        System.out.println(rotateVal);
-        Robot.driveTrain.arcadeDrive(0.0, -rotateVal);
+//        NetworkTable table = NetworkTableInstance.getDefault().getTable("vision");
+//        double input = table.getEntry("ctrX").getDouble(0);
+//        double rotateVal = (input < 0 ? -1 : 1) * Math.sqrt(Math.abs(input));
+//        System.out.println(rotateVal);
+//        Robot.driveTrain.arcadeDrive(0.0, -rotateVal);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -43,6 +92,8 @@ public class FollowCamera extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+        rotatePID.disable();
+        
         // Robot.camera.enableRingLight(false);
         Robot.driveTrain.brakeMode(false);
 
